99396e464e05e0337b1f77d573c8f60179f3f267
lhg0529 <lhg0529@naver.com>
2023-10-12 11:00:52 +0900

    1012

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+
+  let [a,b] = useState('남자 코트 추천');
+  let [d,f] = useState('여자 코트 추천');
+  let [e,k] = useState('혼성 코트 추천');
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{color:'red',fontSize:'55px'}}>블로그 타이틀</h4>
+      </div>
+      <div className="list">
+        <h4>{a}</h4>
+        <p>10월 9일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{d}</h4>
+        <p>10월 9일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{e}</h4>
+        <p>10월 9일 발행</p>
+      </div>
     </div>
   );
 }
